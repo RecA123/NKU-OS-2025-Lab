998569249e7f8ca43becd38fba8b9b81af332335
eprogressing <167550629+eprogressing@users.noreply.github.com>
2025-11-28 17:18:57 +0800

    Add files via upload

---
+++ lab5/user/divzero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ulib.h>
+
+volatile int zero;
+
+int
+main(void) {
+    int result;
+    asm volatile("div %0, %1, %2"
+                 : "=r"(result)
+                 : "r"(1), "r"(zero));
+    cprintf("value is %d.\n", result);
+    panic("FAIL: T.T\n");
+}
